ea53e5313b62d7f1b59986893b34db70f23678f4
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-06-27 09:44:43 -0600

    Update Exersice15_Listeners.java
    Updated Exercise15_Listeners class

--- Chapter 15/Exercise15-Listeners/Exersice15_Listeners.java
+++ Chapter 15/Exercise15-Listeners/Exersice15_Listeners.java
@@ -1,14 +1,37 @@
-package exercise15;
-
-import javafx.application.Application; 
-import javafx.scene.Scene; 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage; 
+import javafx.scene.layout.HBox; 
+import javafx.scene.layout.BorderPane; 
 
 public class Exersice15_Listeners extends Application{
 	@Override
 	public void start(Stage primaryStage) {
 		BallPane ball = new BallPane(); 
-		Scene scene = new Scene(ball, 300, 300);
+		
+		Button left = new Button("Left");
+		left.setOnAction(e -> ball.Left());
+		Button right = new Button("Right");
+		right.setOnAction(e -> ball.Right());
+		Button up = new Button("Up");
+		up.setOnAction(e -> ball.UP());
+		Button down = new Button("Down");
+		down.setOnAction( e -> ball.Down());
+		
+		HBox buttons = new HBox(left, right, up, down);
+		buttons.setAlignment(Pos.TOP_CENTER);
+		buttons.setPadding(new Insets(25, 25, 25, 25));
+		buttons.setSpacing(10);
+		
+		BorderPane pane = new BorderPane();
+		pane.setCenter(ball);
+		pane.setTop(buttons);
+		
+		Scene scene = new Scene(pane, 300, 300);
 		primaryStage.setTitle("Moving Ball");
 		primaryStage.setScene(scene);
 		primaryStage.show(); 
@@ -18,3 +41,4 @@ public class Exersice15_Listeners extends Application{
 		Application.launch(args);
 	}
 }
+
