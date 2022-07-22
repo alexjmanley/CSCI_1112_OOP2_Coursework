a4f4f7e12b519e6361c2309d63ddfd3547f528d7
alexjmanley <105018052+alexjmanley@users.noreply.github.com>
2022-07-22 11:52:16 -0600

    Update Final_Project.java
    Updated final project file.

--- Final Project/Final_Project.java
+++ Final Project/Final_Project.java
@@ -1,32 +1,23 @@
 package finalproject;
 
-import javafx.application.Application; 
+import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene; 
 import javafx.scene.control.*; 
 import javafx.stage.Stage; 
 import javafx.scene.layout.*;
 
 public class Final_Project extends Application{
-	//List of variables 
-	double weight = 0.0;
+	//List of text fields
 	TextField tfweight = new TextField(); 
-	double goalWeight = 0.0;
 	TextField tfgoalWeight = new TextField(); 
-	String gender = "Male";
 	TextField tfgender = new TextField(); 
-	double height = 0.0;
-	TextField tfheight = new TextField(); 
-	int age = 0; 
-	TextField tfage = new TextField(); 
-	double calories = calculateCalories(weight, gender, height, age);
-	int weeksToGoal = 0; 
+	TextField tfheight = new TextField();  
+	TextField tfage = new TextField();  
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// make text field 
-		
-		
-		//Make pane 
+		//Make 1st pane 
 		GridPane pane = new GridPane(); 
 		pane.setHgap(5);
 		pane.setVgap(5);
@@ -34,15 +25,23 @@ public class Final_Project extends Application{
 		pane.add(tfweight, 1, 0);
 		pane.add(new Label("Enter Goal Weight in kg: "), 0, 1);
 		pane.add(tfgoalWeight, 1, 1);
-		pane.add(new Label("Enter your Gender: "), 0, 2);
+		pane.add(new Label("Enter your Gender ex. Male: "), 0, 2);
 		pane.add(tfgender, 1, 2);
 		pane.add(new Label("Enter your height in cm: "), 0, 3);
 		pane.add(tfheight, 1, 3);
 		pane.add(new Label("Enter your age in years: "), 0, 4);
 		pane.add(tfage, 1, 4);
+		Button next = new Button("Next");
+		pane.add(next, 1, 5);
+		
+		//setOnAction for Next Button 
+		next.setOnAction(e -> {
+		Scene scene = new Scene(variablesPane(), 320, 250); 
+		primaryStage.setScene(scene);
+		});
 		
 		// create scene and set stage. 
-		Scene scene = new Scene(pane, 320, 350); 
+		Scene scene = new Scene(pane, 320, 250); 
 		primaryStage.setTitle("MyJavaFX"); 
 		primaryStage.setScene(scene);
 		primaryStage.show(); 
@@ -60,6 +59,38 @@ public class Final_Project extends Application{
 		else 
 			calories = (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)) * 1.55;
 		return calories;
+	}
+	//add variables method 
+	public Pane variablesPane() {
+		//Add variables from original 
+				double weight = Double.parseDouble(tfweight.getText());
+				double goalWeight = Double.parseDouble(tfgoalWeight.getText());
+				String gender = tfgender.getText(); 
+				double height = Double.parseDouble(tfheight.getText());
+				int age = Integer.parseInt(tfage.getText());
+				double calories = calculateCalories(weight, gender, height, age);
+				int weeksToGoal = 0; 
+					if (weight > goalWeight) {
+						calories = calories - 500;
+						while (weight > goalWeight) {
+							weight = weight - 0.5;
+							weeksToGoal++;
+						}
+					} else if (weight < goalWeight) {
+						calories = calories + 500; 
+						while (weight < goalWeight) {
+							weight = weight + 0.5; 
+							weeksToGoal++; 
+						}
+					}
+		//Add Pane
+				Pane pane = new Pane(); 
+				Label label = new Label("Your calorie suggestion is \n" + calories + " calories daily.\n"
+						+ "You will be at your goal weight after " + weeksToGoal + " weeks \n"
+						+ "Your protien suggestion is \n" + (calories * 0.25) / 4 + " grams daily");
+				
+				pane.getChildren().add(label);
+				return pane; 
 	}
 
 }
